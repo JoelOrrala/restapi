7f3f2ebcb089b55445f26cb4a777261f81999216
JoelOrrala <jjorrala@espol.edu.ec>
2024-07-15 08:08:29 -0500

    nuevo

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-require('dotenv').config()
+require("dotenv").config();
 
- const express = require('express');
- const bodyParser = require('body-parser');
- const admin = require('firebase-admin');
- const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_API)
+const express = require("express");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_API);
+/* Referencia al módulo swagger-ui-express */
+const swaggerUi = require("swagger-ui-express");
+/* Referencia al archivo con la descripción */
+const swaggerFile = require("./swagger_output.json");
 
- admin.initializeApp({
-   credential: admin.credential.cert(serviceAccount)
- });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
- const app = express();
- app.use(bodyParser.json());
+const app = express();
+app.use(bodyParser.json());
 
- const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
- app.use('/api', require('./routes/api'));
+/* Ruta Base -> Documentación */
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
- app.listen(PORT, () => {
-   console.log(`Server is running on port ${PORT}`);
- });
+app.use("/api", require("./routes/api"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
